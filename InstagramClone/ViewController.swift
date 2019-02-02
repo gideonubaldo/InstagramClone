4f5e7537309bb6c23ffb9d62dfdc9cba7ba148a1
gideon ubaldo <gideon.ubaldo@sjsu.edu>
2019-02-02 11:24:22 -0800

    base

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Gideon Ubaldo on 2/2/19.
+//  Copyright © 2019 Gideon Ubaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
